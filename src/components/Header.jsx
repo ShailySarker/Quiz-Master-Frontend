d3e785aca3d0a04343f347711bf716fdfde68e2f
ShailySarker <shailysarker99@gmail.com>
2025-02-19 22:27:46 +0600

    Header section added with responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import { FcIdea } from 'react-icons/fc';
 
 const Header = () => {
     return (
-        <div>
-            
+        <div className="sticky top-0 z-50 border-b-2 bg-white border-amber-400 xl:px-24 lg:px-16 md:px-10 px-5 xl:py-6 lg:py-[14px] md:py-4 py-[14px] flex justify-between">
+            <h2 className="font-bold text-amber-400 xl:text-3xl md:text-2xl text-xl flex items-center lg:gap-2 gap-1"><FcIdea className='xl:text-4xl md:text-3xl text-2xl' />Quiz Master</h2>
+            <div className="flex items-center gap-3">
+                <button className='bg-amber-400 xl:py-[10px] lg:py-2 md:py-[6px] py-1 text-white rounded-3xl shadow-md px-4 font-semibold lg:text-base md:text-sm text-[13.8px] lg:w-28 w-24'>Sign Up</button>
+                <button className='hidden md:block border-2 border-amber-400 xl:py-[10px] lg:py-2 md:py-[6px] py-1 text-amber-400 rounded-3xl shadow-md px-4 font-semibold lg:text-base md:text-sm text-[13.8px] lg:w-28 w-24'>Sign In</button>
+            </div>
         </div>
     );
 };
--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -14,7 +14,7 @@ const LandingPage = () => {
     // text-[#FD6F00]
     return (
         <div className='h-screen flex justify-center items-center'>
-            <div className="flex items-center justify-center lg:gap-3 gap-2 text-amber-500 animate-bounce">
+            <div className="flex items-center justify-center lg:gap-3 gap-2 text-amber-400 animate-bounce">
                 <FcIdea className="xl:text-6xl lg:text-5xl md:text-4xl text-3xl" />
                 <h1 className="xl:text-5xl lg:text-4xl md:text-3xl text-2xl font-semibold">Quiz Master</h1>
             </div>
