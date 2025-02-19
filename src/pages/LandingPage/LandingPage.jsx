7a20ec8afd06d3439d4aece7f81229e6f571333b
ShailySarker <shailysarker99@gmail.com>
2025-02-19 21:52:59 +0600

    Landing page added with responsive design

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,5 @@
 import { useEffect } from "react";
+import { FcIdea } from "react-icons/fc";
 import { useNavigate } from "react-router";
 
 const LandingPage = () => {
@@ -6,15 +7,17 @@ const LandingPage = () => {
 
     useEffect(() => {
         const timer = setTimeout(() => {
-            navigate("/posts");
+            navigate("/home");
         }, 3000);
         return () => clearTimeout(timer);
     }, [navigate]);
-
+    // text-[#FD6F00]
     return (
-        <div>
-            <h1 className="text-3xl font-bold underline">
-                LandingPage            </h1>
+        <div className='h-screen flex justify-center items-center'>
+            <div className="flex items-center justify-center lg:gap-3 gap-2 text-amber-500 animate-bounce">
+                <FcIdea className="xl:text-6xl lg:text-5xl md:text-4xl text-3xl" />
+                <h1 className="xl:text-5xl lg:text-4xl md:text-3xl text-2xl font-semibold">Quiz Master</h1>
+            </div>
         </div>
     );
 };
--- src/routes/Routers.jsx
+++ src/routes/Routers.jsx
@@ -1,18 +1,22 @@
 import { Route, Routes } from 'react-router';
 import MainLayouts from '../layouts/MainLayouts';
 import LandingPage from '../pages/LandingPage/LandingPage';
+import Home from '../pages/Home/Home';
 
 const Routers = () => {
     return (
         <Routes>
             <Route
+                index
+                element={<LandingPage />}
+            />
+            <Route
                 path="/"
                 element={<MainLayouts />}
             >
                 <Route
-                    // path="/"
-                    index
-                    element={<LandingPage />}
+                     path="/home"
+                    element={<Home />}
                 />
             </Route>
         </Routes>
